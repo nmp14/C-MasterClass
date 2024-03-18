0e1a4aa5341272d1858560c782e73d74f4932fbf
Nathan Peek <nmp14fsu@gmail.com>
2024-03-17 22:13:12 -0400

    Refactor and complete TODO test app

--- TodoList/Program.cs
+++ TodoList/Program.cs
@@ -2,54 +2,149 @@
 {
     private static void Main(string[] args)
     {
-        while (true)
+        bool shouldExit = false;
+        TodoManager todoManager = new TodoManager();
+
+        do
         {
-            PromptAndPrintUserChoiceForTODOList();
-        }
+            string userInput = todoManager.PromptTodoChoices();
 
-        void PrintSelectedOption(string message)
-        {
-            Console.WriteLine("\nSelected option: " + message + "\n");
-        }
-
-        void PromptAndPrintUserChoiceForTODOList()
-        {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("What would you like to do? :)");
-            Console.WriteLine("[S]ee all TODOs");
-            Console.WriteLine("[A]dd a TODO");
-            Console.WriteLine("[R]emove a TODO");
-            Console.WriteLine("[E]xit\n");
-
-            string userChoice = Console.ReadLine().ToUpper();
-
-            if (userChoice?.Length > 0)
+            switch(userInput.ToUpper())
             {
-                if (userChoice == "S")
-                {
-                    PrintSelectedOption("See all TODOs");
-                }
-                else if (userChoice == "A")
-                {
-                    PrintSelectedOption("Add a TODO");
-                }
-                else if (userChoice == "R")
-                {
-                    PrintSelectedOption("Remove a TODO");
-                }
-                else if (userChoice == "E")
-                {
-                    Environment.Exit(0);
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice\n");
-                }
+                case "S":
+                    todoManager.ListAllTodos();
+                    break;
+                case "A":
+                    todoManager.AddTodo();
+                    break;
+                case "R":
+                    todoManager.RemoveTodo();
+                    break;
+                case "E":
+                    shouldExit = true;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option! Try again.\n");
+                    break;
             }
-            else
+
+
+        } while (!shouldExit);
+    }
+}
+
+public class TodoManager
+{
+    private List<string> TodoList = new List<string>();
+
+    public string PromptTodoChoices()
+    {
+        Console.WriteLine("Hello!");
+        Console.WriteLine("What would you like to do? :)");
+        Console.WriteLine("[S]ee all TODOs");
+        Console.WriteLine("[A]dd a TODO");
+        Console.WriteLine("[R]emove a TODO");
+        Console.WriteLine("[E]xit\n");
+
+        string userInput = Console.ReadLine()!;
+
+        return userInput;
+    }
+
+    public void AddTodo()
+    {
+        string todo;
+        bool isUnique = true;
+
+        do
+        {
+            Console.WriteLine("Please enter unique description");
+            todo = Console.ReadLine();
+
+            if (todo.Length == 0)
             {
-                Console.WriteLine("Did not provide input of valid length. Try again\n");
+                Console.WriteLine("\nThe description cannot be empty.\n");
+                continue;
             }
+
+            if (this.TodoList.Contains(todo))
+            {
+                Console.WriteLine("The description must be unique.\n");
+                isUnique = false;
+                continue;
+            }
+
+            break;
+        } while (todo.Length == 0 || !isUnique);
+
+        this.TodoList.Add(todo);
+        Console.WriteLine($"TODO successfully added: {todo}\n");
+    }
+
+    public void RemoveTodo()
+    {
+        this.ListAllTodos();
+
+        int count = this.TodoList.Count;
+
+        if (count == 0) return;
+
+        bool shouldRepeat = false;
+        do
+        {
+            Console.WriteLine("Enter number of TODO you wish to remove");
+
+            string idx = Console.ReadLine();
+
+            if (idx.Length == 0)
+            {
+                Console.WriteLine("Selected index cannot be empty.");
+                shouldRepeat = true;
+                continue;
+            }
+
+            bool successfullyParsed = int.TryParse(idx, out int parsedIdx);
+
+            if (!successfullyParsed)
+            {
+                Console.WriteLine("The given index is not valid.");
+                shouldRepeat = true;
+                continue;
+            }
+
+            if (parsedIdx > count || parsedIdx < 1)
+            {
+                Console.WriteLine("The given index is not valid. Out of range.");
+                shouldRepeat = true;
+                continue;
+            }
+
+            string todo = this.TodoList[parsedIdx - 1];
+            this.TodoList.RemoveAt(parsedIdx - 1);
+
+            Console.WriteLine($"TODO removed: {todo}\n");
+
+            shouldRepeat = false;
+        } while (shouldRepeat);
+    }
+
+    public void ListAllTodos()
+    {
+        int count = this.TodoList.Count;
+
+        if (count < 1)
+        {
+            Console.WriteLine("\nNo TODOs have been added yet.\n");
+            return;
         }
+
+        Console.WriteLine("");
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {this.TodoList[i]}");
+        }
+
+        Console.WriteLine("");
     }
 }
