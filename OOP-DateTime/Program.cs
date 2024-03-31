208f99df63e530cb2036a7a887ed3c623e409154
Nathan Peek <nmp14fsu@gmail.com>
2024-03-31 19:34:48 -0400

    Test dates

--- OOP-DateTime/Program.cs
+++ OOP-DateTime/Program.cs
@@ -3,5 +3,26 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine(new DateTime(2024, 3, 31));
+        Console.WriteLine(DateTime.Now.ToUniversalTime());
+        Console.WriteLine("\n");
+
+        DateTime internationalPizzaDay2023 = new DateTime(2023, 3, 31);
+
+        Console.WriteLine("Year is " + internationalPizzaDay2023.Year);
+        Console.WriteLine("Month is " + internationalPizzaDay2023.Month);
+        Console.WriteLine("Day is " + internationalPizzaDay2023.Day);
+        Console.WriteLine("Day of the week is " + internationalPizzaDay2023.DayOfWeek);
+        Console.WriteLine("\n");
+
+        DateTime internationalPizzaDay2024 = internationalPizzaDay2023.AddYears(1);
+
+        Console.WriteLine("Year is " + internationalPizzaDay2024.Year);
+        Console.WriteLine("Month is " + internationalPizzaDay2024.Month);
+        Console.WriteLine("Day is " + internationalPizzaDay2024.Day);
+        Console.WriteLine("Day of the week is " + internationalPizzaDay2024.DayOfWeek);
+        Console.WriteLine("\n");
+
+        Console.ReadKey();
     }
 }
